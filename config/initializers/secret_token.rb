5f88d2b1760b07c2237a99a51775f42a7b4409f3
unknown <slack.dick@gmail.com>
2013-11-06 19:10:45 +0700

    initial commit for first test app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mybook::Application.config.secret_token = 'c57e2243a85a5c6d0a0d74b2aedaa1c4e7b1f791bf63ddbbf8871171ace6d20cfc38a321a26b861713154f67454c5fc2167576a8b5fa94bba14e3eb82609358e'
